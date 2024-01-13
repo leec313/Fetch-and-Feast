0aa304b57528f02774673061723f1b1e04763ad0
Lee Curtis <leec313@gmail.com>
2024-01-13 11:58:05 +0000

    Removed secret key and set debug

--- fetch_and_feast/settings.py
+++ fetch_and_feast/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e5%xahk%(mh&m)te)-gn^o%iipq#eom7k6j7$(idfvy#6s42cd'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-leec313-fetchandfeast-8b89e9jrgzv.ws-eu107.gitpod.io', 'fetch-and-feast-4ceb13480b0c.herokuapp.com']
 

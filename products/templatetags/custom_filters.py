00aba993d63a87fac6d992f4e3597df50a1f0793
Lee Curtis <leec313@gmail.com>
2024-01-26 22:22:56 +0000

    Added average rating to product detail page

--- products/templatetags/custom_filters.py
+++ products/templatetags/custom_filters.py
@@ -19,3 +19,23 @@ def get_profile_image_url(user_id, rating_profiles):
     If the user ID is not found in the dictionary, return an empty string.
     """
     return rating_profiles.get(user_id, '')
+
+
+@register.filter(name='stars')
+def stars(value):
+    """Round the average rating to the nearest whole number
+    and display as star icons from Font Awesome"""
+    rounded_value = round(value)
+
+    # Create a string with Font Awesome icons
+    stars_string = ''.join([
+        '<i class="fas fa-star fas-star-solid"></i>' for _ in range(
+            rounded_value)
+    ])
+
+    stars_string += ''.join([
+        '<i class="far fa-star far-star-outline"></i>' for _ in range(
+            5 - rounded_value)
+    ])
+
+    return stars_string

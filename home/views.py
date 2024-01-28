9acc7b3e3a2dedb2aad2ed010c3bd9ffacd2bd87
Lee Curtis <leec313@gmail.com>
2024-01-28 14:25:15 +0000

    Added featured products logic and html/css

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Avg
+from products.models import Product
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    """Get top-rated products. From the rating model in the product app
+    get the average rating and order by highest. Only show first 4"""
+    featured_products = Product.objects.annotate(
+        avg_rating=Avg('rating__score')).order_by('-avg_rating')[:4]
+    return render(
+        request, 'home/index.html', {'featured_products': featured_products})

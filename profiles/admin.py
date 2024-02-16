eb4dbf4fdb7824c26901dffc08c19bcee1b49280
Lee Curtis <leec313@gmail.com>
2024-02-16 20:56:06 +0000

    Added logic to hide profiles w/o username

--- profiles/admin.py
+++ profiles/admin.py
@@ -7,6 +7,11 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = (
         'user', 'default_phone_number', 'default_country', 'profile_image')
 
+    # Hide any profiles without a username/null
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.exclude(user__username=None)
+
 
 @admin.register(NewsletterSubscription)
 class NewsletterSubscriptionAdmin(admin.ModelAdmin):
